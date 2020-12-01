79f730cc04cfa0be2fe77bd1affdb840456de627
atais <atais.jr@gmail.com>
2020-12-01 17:19:05 +0100

    trying to reanimate the tests

---
+++ jest/reanimated.js
@@ -0,0 +1,2 @@
+jest.mock('react-native-reanimated',
+  () => require('react-native-reanimated/mock'));
---
+++ jest/track-player.js
@@ -0,0 +1,36 @@
+jest.mock('react-native-track-player', () => {
+  return {
+    addEventListener: jest.fn(),
+    registerEventHandler: jest.fn(),
+    registerPlaybackService: jest.fn(),
+    setupPlayer: jest.fn(),
+    destroy: jest.fn(),
+    updateOptions: jest.fn(),
+    add: jest.fn(),
+    remove: jest.fn(),
+    skip: jest.fn(),
+    skipToNext: jest.fn(),
+    skipToPrevious: jest.fn(),
+    removeUpcomingTracks: jest.fn(),
+    // playback commands
+    reset: jest.fn(),
+    play: jest.fn(),
+    pause: jest.fn(),
+    stop: jest.fn(),
+    seekTo: jest.fn(),
+    setVolume: jest.fn(),
+    setRate: jest.fn(),
+    // player getters
+    getQueue: jest.fn(),
+    getTrack: jest.fn(),
+    getCurrentTrack: jest.fn(),
+    getVolume: jest.fn(),
+    getDuration: jest.fn(),
+    getPosition: jest.fn(),
+    getBufferedPosition: jest.fn(),
+    getState: jest.fn(),
+    getRate: jest.fn(),
+    // state
+    usePlaybackState: jest.fn(),
+  };
+});

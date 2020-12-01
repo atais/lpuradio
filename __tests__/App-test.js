79489bba086102d6b1191a74a15ffdc6de402e47
atais <atais.jr@gmail.com>
2020-12-01 16:19:11 +0100

    the tests dont work, though

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,11 @@
 
 import 'react-native';
 import React from 'react';
-import BackupApp from '../components/LpuRadio';
+import App from '../components/Main';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<BackupApp />);
+  renderer.create(<App />);
 });
